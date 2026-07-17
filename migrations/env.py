5381eb69857ac1fcbb05b396e1105004b3cd66d2
Alena <alena.kozlova2004@mail.ru>
2026-07-17 18:43:57 +0300

    init migration

---
+++ migrations/env.py
@@ -0,0 +1,45 @@
+import asyncio
+from logging.config import fileConfig
+
+from sqlalchemy.ext.asyncio import create_async_engine
+from alembic import context
+
+from app.core.config import settings
+from app.models.models import Base
+
+config = context.config
+fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+
+def run_migrations_offline() -> None:
+    context.configure(
+        url=settings.db_url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_migrations_online() -> None:
+    connectable = create_async_engine(settings.db_url)
+    async with connectable.connect() as connection:
+        await connection.run_sync(
+            lambda conn: context.configure(
+                connection=conn,
+                target_metadata=target_metadata,
+                compare_type=True,
+            )
+        )
+        async with connection.begin():
+            await connection.run_sync(lambda _: context.run_migrations())
+    await connectable.dispose()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    asyncio.run(run_migrations_online())
